db9678c3f77dc4f2dcfb6764daf723532647510d
deverober <robthedeveloper@icloud.com>
2022-06-12 19:43:57 +0200

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,23 @@
 import { EmitterObject } from "./objects/EmitterObject";
 import { EventManager } from "./events/EventManager";
 
-const eventManager = EventManager.getInstance();
+// const eventManager = EventManager.getInstance();
 
-const obj = new EmitterObject();
-const obj1 = new EmitterObject();
+// const obj = new EmitterObject();
+// const obj1 = new EmitterObject();
 
-obj.subscribeToTargettedEvent({
-	name: "test",
-	callback: () => {
-		console.log("Test passed in obj.");
-	},
-});
-
-obj1.subscribeToTargettedEvent({
-	name: "test",
-	callback: () => {
-		console.log("Test passed in obj1");
-	},
-});
-
-EventManager.emitToAll("test");
-
-// EventManager.emitToOne({
-// 	callerId: obj._id,
-// 	targetId: obj1._id,
-// 	eventName: "add",
+// obj.subscribeToTargettedEvent({
+// 	name: "test",
+// 	callback: () => {
+// 		console.log("Test passed in obj.");
+// 	},
 // });
 
-// const obj2 = new EmitterObject([emitterEvent]);
+// obj1.subscribeToTargettedEvent({
+// 	name: "test",
+// 	callback: () => {
+// 		console.log("Test passed in obj1");
+// 	},
+// });
 
-// obj.emitter.emit("test");
+// EventManager.emitToAll("test");
